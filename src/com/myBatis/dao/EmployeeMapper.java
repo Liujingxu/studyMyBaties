f697390e5317a6f47ef5050d951a35074ca9daf5
ljx_glb <1913154305@qq.com>
2019-02-28 19:08:16 +0800

    study myBatis helloWorld version3.10
    test crud
    
    date: 2019_02_28_19_08

--- src/com/myBatis/dao/EmployeeMapper.java
+++ src/com/myBatis/dao/EmployeeMapper.java
@@ -5,4 +5,10 @@ import com.myBatis.entity.Employee;
 public interface EmployeeMapper {
 
     public Employee getEmpById(Integer id);
+
+    public void insertEmp(Employee employee);
+
+    public boolean deleteEmp(Integer id);
+
+    public Integer updateEmp(Employee employee);
 }
--- test/com/myBatis/entity/EmployeeTest.java
+++ test/com/myBatis/entity/EmployeeTest.java
@@ -46,7 +46,7 @@ class EmployeeTest {
 
         try {
             EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
-            Employee employee = mapper.getEmpById(1713010614);
+            Employee employee = mapper.getEmpById(1713010618);
             System.out.println(employee);
         }finally {
             sqlSession.close();
@@ -66,6 +66,52 @@ class EmployeeTest {
         } finally {
             sqlSession.close();
         }
+    }
+
+    @Test
+    public void test4() throws IOException {
+
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactoryl();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+
+        try {
+            mapper.insertEmp(new Employee(1713010618, "Ezio", "ezio@qq.com", "1"));
+            sqlSession.commit();
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void test5() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactoryl();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+
+            System.out.println(mapper.deleteEmp(1713010618));
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+
+    @Test
+    public void test6() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactoryl();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+
+        try {
+            Integer emp = mapper.updateEmp(new Employee(1713010615, "Yammy", "yammy@qq.com", "0"));
+            sqlSession.commit();
+            System.out.println("there are " + emp + " changes");
+        } finally {
+            sqlSession.close();
+        }
+
     }
 
 
